5e03998c23fab8fa35066cd941c3310ab68cd82c
Nguyen Van Phong <nguyenphong.pn714@gmail.com>
2017-12-15 17:05:48 +0700

    update google map and splash screen by nguyen van phong date 15/12/2017

--- Googlemap/app/src/main/java/com/example/phong/googlemap/MainActivity.java
+++ Googlemap/app/src/main/java/com/example/phong/googlemap/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.phong.googlemap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.wang.avi.AVLoadingIndicatorView;
+
+/**
+ * Created by Nguyen Van Phong on 12/15/2017.
+ */
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    Animation slidedown;
+    ImageView imgLogo;
+    Handler handler = new Handler();
+    AVLoadingIndicatorView avi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        slidedown = AnimationUtils.loadAnimation(MainActivity.this,R.anim.slider_down);
+        avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
+        imgLogo = (ImageView) findViewById(R.id.logo);
+        avi.show();
+        imgLogo.startAnimation(slidedown);
+        if(isServicesOK()){
+            init();
+        }
     }
+
+    private void init(){
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkConnection();
+            }
+        }, 3000);
+    }
+
+    private void checkConnection(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            avi.hide();
+            imgLogo.clearAnimation();
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Toast.makeText(MainActivity.this,"Internet not available!!!",Toast.LENGTH_SHORT).show();
+            avi.hide();
+            imgLogo.clearAnimation();
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 }
